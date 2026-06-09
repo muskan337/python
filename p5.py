b15b0d9021f4177ac9e74a6c5188a160dcfca9ad
muskan337 <jainmuskan.06.07@gmail.com>
2026-06-09 16:18:24 +0530

    learning python

---
+++ p5.py
@@ -0,0 +1,80 @@
+#        LOOP- ued to repeat a block of code a certain number of times --> while or for loop
+# while loop-
+count = 1
+# while count <= 5:
+
+    # print("hello")
+    # count += 1
+    # print(count)
+
+    # i = 1
+    # while i<= 5:
+    #     print(i)
+    #     i +=1
+
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+# i = 100
+# while i>0:
+#  print(i)
+#  i -= 1
+
+# n = int(input("enter a number:"))
+# i = 1
+# while i<= 10:
+#  print(i*n)
+#  i = i+1
+
+# i = 1
+# while i<= 10:
+#     print( i*i)
+#     i +=1
+
+# tupple = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+# i = int(input("enter a number:"))
+# j = 0
+# while j< len(tupple):
+#     if(tupple[j] == i):
+#         print(tupple[j])
+#     else:
+#         print("not found")
+#         j += 1
+    
+# i =1
+# while i<= 5:
+#     print(i)
+#     if(i == 3):
+#         break 
+#     i+=1
+
+# i = 1
+# while i <= 5:
+#     if(i == 3):
+#         i += 1
+#         continue
+#     # print(i)
+#     i+=1
+
+# list = [1, 2, 3]
+
+# for el in list:
+#     print(el)
+
+# tup = (1, 2, 3)
+# for el in tup:
+#     print(el)
+# else:
+#     print("end")
+
+str = "mukan jain"
+
+for char in str:
+    if(char == "a"):
+        print("o, found")
+        break
+    print("not found")
+else: 
+    print("END")
